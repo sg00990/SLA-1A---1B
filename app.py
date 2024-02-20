3a61ec3e348489c6f707eab943625805344be3fd
Sarah Graddy <graddysa@icloud.com>
2024-02-20 14:58:44 -0500

    edits

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
-import time
+import datetime
+import json
 
 st.set_page_config(
     page_title="Tier 2 SLA Questionnaire",
     page_icon="💻",
     layout="wide"
 )
+
+conn = st.connection("snowflake")
 
 st.markdown('<p style="font-family:sans-serif; color:#324a62; font-size: 28px; font-weight: bold">Tier 2 SLA Questionnaire</p>', unsafe_allow_html=True)
 st.write("###")
@@ -15,6 +18,12 @@ st.markdown('<p style="font-family:sans-serif; color:#87c440; font-size: 20px; f
 
 st.write("**Did your team experience a Planned or Unplanned Outage this past week?**")
 experienced_outage = st.radio("experienced_outage", options=["No", "Yes"], label_visibility="collapsed")
+
+outage_start_datetime = None
+outage_end_datetime = None
+outage_desc = None
+workstream = None
+outage_planned = None
 
 if experienced_outage == "Yes":
     st.write("**If yes, please answer the following**")
@@ -42,19 +51,42 @@ if experienced_outage == "Yes":
         outage_end_time = st.time_input("Outage End Time", step=60)
 
     outage_desc = st.text_area("Brief Description", key="outage_desc")
+    outage_desc = outage_desc.replace("\n", "  ").replace("'", "''").replace('"', r'\"')
+
+    outage_start_datetime = datetime.datetime.combine(outage_start, outage_start_time)
+    outage_end_datetime = datetime.datetime.combine(outage_end, outage_end_time)
 
 
 st.write("**Date**")
 survey_date = st.date_input("survey_date", format="MM/DD/YYYY", label_visibility="collapsed")
 st.write("**Additional Comments**")
 survey_text = st.text_area("survey_text", label_visibility="collapsed")
+survey_text = survey_text.replace("\n", "  ").replace("'", "''").replace('"', r'\"')
 
 
 col1, col2, col3 = st.columns(3)
 
 with col3:
     if st.button("Submit", use_container_width=True):
-        st.success("Thank you for your responses!")
+        data = {
+            "sla_1ab_experienced_outage": experienced_outage,
+            "sla_1ab_workstream": workstream,
+            "sla_1ab_outage_planned": outage_planned,
+            "sla_1ab_outage_start": outage_start_datetime,
+            "sla_1ab_outage_end": outage_end_datetime,
+            "sla_1ab_outage_reason": outage_desc,
+            "sla_1ab_date": survey_date,
+            "sla_1ab_comments": survey_text
+        }
+
+        json_data = json.dumps(data, indent=4, sort_keys=True, default=str)
+
+        date_submitted = datetime.datetime.now()
+
+        try:
+            conn.query(f""" INSERT INTO sla_tier_2_questionnaire (type, date_submitted, json_data) SELECT 'SLA 1A & 1B', '{date_submitted}', (parse_json('{json_data}'))""")
+        except:
+            st.success("Thank you for your responses!")
 
 
 col4, col5, col6 = st.columns([1, .5, 1])
