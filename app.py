2d11853a2ef3eddc542a3407b0ea2cb8cfaab03d
Sarah Graddy <graddysa@icloud.com>
2024-02-21 15:10:55 -0500

    time input, workstream

--- app.py
+++ app.py
@@ -18,16 +18,16 @@ st.markdown('<p style="font-family:sans-serif; color:#87c440; font-size: 20px; f
 
 st.write("**Did your team experience a Planned or Unplanned Outage this past week?**")
 experienced_outage = st.radio("experienced_outage", options=["No", "Yes"], label_visibility="collapsed")
+st.write("**Workstream**")
+workstream = st.selectbox("Workstream", options=["BP", "Impact", "LDAP", "Doc Center", "CDMS", "IC Importers", "Content Manager"], label_visibility="collapsed")
 
 outage_start_datetime = None
 outage_end_datetime = None
 outage_desc = None
-workstream = None
 outage_planned = None
 
 if experienced_outage == "Yes":
     st.write("**If yes, please answer the following**")
-    workstream = st.selectbox("Workstream", options=["BP", "Impact", "LDAP", "Doc Center"])
     outage_planned = st.radio("Planned?", options=["No", "Yes"])
 
     col3, col4 = st.columns(2)
@@ -38,7 +38,7 @@ if experienced_outage == "Yes":
             format="MM/DD/YYYY"
         )
     with col4:
-        outage_start_time = st.time_input("Outage Start Time", step=60)
+        outage_start_time = st.time_input("Outage Start Time", step=60, value=None, help="Please type or select a time from the list. To restart, please select the 'x' to the right.")
 
     col5, col6 = st.columns(2)
 
@@ -48,17 +48,16 @@ if experienced_outage == "Yes":
             format="MM/DD/YYYY",
         )
     with col6:
-        outage_end_time = st.time_input("Outage End Time", step=60)
+        outage_end_time = st.time_input("Outage End Time", step=60, value=None, help="Please type or select a time from the list. To restart, please select the 'x' to the right.")
 
     outage_desc = st.text_area("Brief Description", key="outage_desc")
     outage_desc = outage_desc.replace("\n", "  ").replace("'", "''").replace('"', r'\"')
 
-    outage_start_datetime = datetime.datetime.combine(outage_start, outage_start_time)
-    outage_end_datetime = datetime.datetime.combine(outage_end, outage_end_time)
+    if outage_start_time is not None:
+        outage_start_datetime = datetime.datetime.combine(outage_start, outage_start_time)
+    if outage_end_time is not None:
+        outage_end_datetime = datetime.datetime.combine(outage_end, outage_end_time)
 
-
-st.write("**Date**")
-survey_date = st.date_input("survey_date", format="MM/DD/YYYY", label_visibility="collapsed")
 #st.write("**Additional Comments**")
 #survey_text = st.text_area("survey_text", label_visibility="collapsed")
 #survey_text = survey_text.replace("\n", "  ").replace("'", "''").replace('"', r'\"')
@@ -75,7 +74,6 @@ with col3:
             "sla_1ab_outage_start": outage_start_datetime,
             "sla_1ab_outage_end": outage_end_datetime,
             "sla_1ab_outage_reason": outage_desc,
-            "sla_1ab_date": survey_date,
             #"sla_1ab_comments": survey_text
         }
 
